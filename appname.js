9f949f3a7a425bf2475ab07c6c7c48e7c9826e5f
nanodeep <deepreze@gmail.com>
2017-03-06 08:44:39 +0530

    added local storage

--- appname.js
+++ appname.js
@@ -1,13 +1,25 @@
-      var app = angular.module('myfirstapp', []);
+var app = angular.module('myfirstapp', []);
 app.controller('namectrlr', function($scope) {
-$scope.nameList = [];
+$scope.nameList2 = JSON.parse(localStorage.getItem('nameList1'));
 $scope.addthename = function(modeItem){
  if (!(modeItem === undefined || modeItem === '')){
-     $scope.nameList.push(modeItem);     
-     } 
+     $scope.nameList2.push(modeItem);
+     }
+
+ localStorage.setItem('nameList1',JSON.stringify($scope.nameList2));
+
 }
+
  $scope.remove=function(i){ 
-      var index=$scope.nameList.indexOf(i)
-      $scope.nameList.splice(index,1);     
+      var index=$scope.nameList2.indexOf(i)
+      $scope.nameList2.splice(index,1);
+     localStorage.setItem('nameList1',JSON.stringify($scope.nameList2));
     }
+
+    $scope.edit=function(index,i){
+        $scope.nameList2[index]=i;
+        localStorage.setItem('nameList1',JSON.stringify($scope.nameList2));
+    }
+
+
 });
